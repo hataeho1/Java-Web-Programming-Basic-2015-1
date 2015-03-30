54affd1e67632890432dbed1c256dd8790dc6832
ho <hataeho1@naver.com>
2015-03-30 17:35:48 +0900

    과제1 완료

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -1,5 +1,9 @@
 package core.ref;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import next.model.Question;
 
 import org.junit.Test;
@@ -11,7 +15,35 @@ public class ReflectionTest {
 	
 	@Test
 	public void showClass() {
+		logger.debug("###show classes");
 		Class<Question> clazz = Question.class;
 		logger.debug(clazz.getName());
+	}
+	
+	@Test
+	public void getAllFileds() throws Exception {
+		logger.debug("###show All Fileds");
+		Class<Question> clazz = Question.class;
+		for (Field filed : clazz.getDeclaredFields()) {
+			logger.debug(filed.getName());
+		}
+	}
+	
+	@Test
+	public void getAllConstructer() throws Exception {
+		logger.debug("###show All Constructer");
+		Class<Question> clazz = Question.class;
+		for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+			logger.debug(constructor.getName());
+		}
+	}
+	
+	@Test
+	public void getAllMethod() throws Exception {
+		logger.debug("###show All Method");
+		Class<Question> clazz = Question.class;
+		for (Method methods : clazz.getDeclaredMethods()) {
+			logger.debug(methods.getName());
+		}
 	}
 }
