a64b54796b54ab0aff6cda942d7df31ccf155cb8
ho <hataeho1@naver.com>
2015-03-30 18:32:52 +0900

    과제3 완료

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,11 +1,31 @@
 package core.ref;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 
 public class Junit4TestRunner {
+
 	@Test
 	public void run() throws Exception {
 		Class<Junit4Test> clazz = Junit4Test.class;
+		for (Method method : clazz.getDeclaredMethods()) {
+			for (Annotation annotation : method.getAnnotations()) {
+				if (annotation.annotationType().getSimpleName().equals("MyTest")) {
+					method.invoke(clazz.newInstance());
+				}
+			}
+		}
+	}
 
+	@Test
+	public void run2() throws Exception {
+		Class<Junit4Test> clazz = Junit4Test.class;
+		for (Method method : clazz.getDeclaredMethods()) {
+			if (method.isAnnotationPresent(MyTest.class)) {
+				method.invoke(clazz.newInstance());
+			}
+		}
 	}
 }
