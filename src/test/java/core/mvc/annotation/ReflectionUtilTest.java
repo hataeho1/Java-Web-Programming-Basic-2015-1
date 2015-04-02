4a156cf9778b69d0b94459144ae89dbb9108c34f
ho <hataeho1@naver.com>
2015-04-02 17:31:20 +0900

    과제 4 - 힌트 2 완료

--- src/test/java/core/mvc/annotation/ReflectionUtilTest.java
+++ src/test/java/core/mvc/annotation/ReflectionUtilTest.java
@@ -1,5 +1,6 @@
 package core.mvc.annotation;
 
+import java.lang.reflect.Method;
 import java.util.Set;
 
 import org.junit.Test;
@@ -16,6 +17,33 @@ public class ReflectionUtilTest {
 		Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(Bean.class);
 		for (Class<?> each : annotated) {
 			logger.debug(each.getName());
+		}
+	}
+	
+	@Test
+	public void 힌트1_Controller_어노테이션이_붙은_클래스를_몽땅_찾는다() {
+	        Reflections reflections = new Reflections("core", "next");
+		Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(Controller.class);
+		for (Class<?> each : annotated) {
+			logger.debug(each.getName());
+		}
+	}
+	
+	@Test
+	public void 힌트2_힌트1에서_찾은_클래스에서_Inject_어노테이션이_붙은_메소드를_몽땅_찾는다() {
+	        Reflections reflections = new Reflections("core", "next");
+		Set<Class<?>> annotatedWithBean = reflections.getTypesAnnotatedWith(Bean.class);
+		Set<Class<?>> annotatedWithController = reflections.getTypesAnnotatedWith(Controller.class);
+		findMethodWithInjectAnnotation(annotatedWithBean);
+		findMethodWithInjectAnnotation(annotatedWithController);
+	}
+
+	private void findMethodWithInjectAnnotation(Set<Class<?>> annotatedWithBean) {
+		for (Class<?> each : annotatedWithBean) {
+			for (Method metd : each.getDeclaredMethods()) {
+				if(!metd.isAnnotationPresent(Inject.class)) continue;
+				logger.debug(metd.getName());
+			}
 		}
 	}
 }
